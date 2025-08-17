9c0fc3d5d5039e24d5e6fa7ec614bfd020d2e100
EdgarAndrade8 <efma15@hotmail.com>
2025-08-17 17:43:45 +0100

    Caesar Cypher
    This is a Caeser Cypher, a tool to encrypt or decrypt messages.

---
+++ Caesar Cypher.py
@@ -0,0 +1,28 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(original_text, shift_amount, encode_or_decode):
+    output_text = ""
+    if encode_or_decode == "decode":
+        shift_amount *= -1
+    for letter in original_text:
+        if letter in alphabet:
+            shifted_position = alphabet.index(letter) + shift_amount
+            shifted_position %= len(alphabet)
+            output_text += alphabet[shifted_position]
+        else:
+            output_text += letter
+    print(f"Here is the {encode_or_decode}d result: {output_text}")
+
+restart = True
+while restart:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+    restart == False
+    wants_restart = input("Do you want to run the program again? Type 'Yes' or 'No'.\n").lower()
+    if wants_restart == "no":
+        restart = False
+        print("Goodbye.")
+    elif wants_restart == "yes":
+        restart == True
